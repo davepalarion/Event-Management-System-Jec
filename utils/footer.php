3a978de1cbf3c1d238a944f3cac89021d7232c60
Dave Palarion <davep@MSI>
2022-09-03 11:27:03 +0800

    Adding Files

---
+++ utils/footer.php
@@ -0,0 +1,55 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+    <footer style="background-color: #71acaf;">
+    <div class="container p-4">
+      <div class="row">
+        <div class="col-lg-6 col-md-12 mb-4">
+          <h5 class="mb-3" style="letter-spacing: 2px; color: #818963; font-size:20px; font-weight: bold;">Event Management System</h5>
+          <p style="color: #4f4f4f; text-align:justify;">
+          Event management is the application of project management to the creation
+          and development of small and/or large-scale personal or corporate events such as 
+          festivals, conferences, ceremonies, weddings, formal parties, concerts, or conventions.
+          </p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4">
+          <h5 class="mb-3" style="letter-spacing: 2px; color: #818963; font-weight: bold;">Links</h5>
+          <ul class="list-unstyled mb-0">
+            <li class="mb-1">
+              <a href="index.php" style="color: #4f4f4f;">Homepage</a>
+            </li>
+            <li class="mb-1">
+              <a href="viewEvents.php" style="color: #4f4f4f; ">View Events</a>
+            </li>
+            <li class="mb-1">
+              <a href="viewLocations.php" style="color: #4f4f4f;">Our Locations</a>
+            </li>
+            <li>
+              <a href="contact.php" style="color: #4f4f4f;">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4">
+          <h5 class="mb-1" style="letter-spacing: 2px; color: #818963; font-weight: bold;">Opening Hours</h5>
+          <table class="table" style="color: #4f4f4f; border-color: #666;">
+            <tbody>
+              <tr>
+                <td>Mon - Fri:</td>
+                <td>8am - 9pm</td>
+              </tr>
+              <tr>
+                <td>Sat - Sun:</td>
+                <td>8am - 1am</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2022 Copyright:
+      EventManager.com
+    </div>
+    <!-- Copyright -->
+  </footer>
+  
+</div>
+<!-- End of .container -->
